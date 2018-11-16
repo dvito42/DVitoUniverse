55b9f03509c05ac8187b5898e57bfafeffa82a5c
dvito42 <dvito42@yahoo.com>
2018-11-16 15:36:32 -0500

    Add project files.

---
+++ DVitoUniverse/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DVitoUniverse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Web Developer Portfolio.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact information.";
+
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            ViewBag.Message = "Resume.";
+
+            return View();
+        }
+
+
+    }
+}
